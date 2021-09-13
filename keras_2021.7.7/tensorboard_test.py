ac06e3088edef78df314ae2949ec8e739ec8ccd8
陈雨 <chenyu_13820592380@126.com>
2021-09-14 01:45:28 +0800

    keras

---
+++ keras_2021.7.7/tensorboard_test.py
@@ -0,0 +1,42 @@
+#  -*-coding:utf8 -*-
+
+"""
+Created on 2021 7 7
+
+@author: 陈雨
+"""
+
+'''使用 MNIST 数据集。接下来编写一个函数对数据进行标准化，
+同时创建一个简单的Keras模型使图像分为10类'''
+import tensorflow as tf
+import datetime
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+def create_model():
+  return tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
+
+model = create_model()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(x=x_train, 
+          y=y_train, 
+          epochs=5, 
+          validation_data=(x_test, y_test), 
+          callbacks=[tensorboard_callback])
+
+# tensorboard --logdir logs/fit 
+# 通过命令行 （command） 或在 notebook 体验中启动 TensorBoard 
